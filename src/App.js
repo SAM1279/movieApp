ebe6b8a8a1532c33d3572795838a0ed6567b5e7f
SAM1279 <saraatef268@gmail.com>
2024-01-20 13:33:50 +0200

    my movie app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Wishlist from "./Components/Wishlist";
+import MovieList from "./Components/MovieList";
+import MovieDetails from "./Components/MovieDetails";
+
+import "./css/navbar.css";
+import "./css/Search.css";
+import "./css/MoviList.css";
+import "./css/MovieDetails.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/movies" element={<MovieList />} />
+        <Route path="/movieDetails/:id" element={<MovieDetails />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
